119ebab8779c4caee61bb301f04d70d109093efc
Brayden Traas <btraas@gmail.com>
2016-11-12 21:18:57 -0800

    commit 2

---
+++ Assignment2B/src/ca/bcit/comp2526/a2b/WaterSquareCell.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2526.a2b;
+
+import java.awt.Color;
+
+@SuppressWarnings("serial")
+public class WaterSquareCell extends SquareCell implements WaterCell {
+
+	public WaterSquareCell(SquareCell origin) {
+		super(origin.getWorld(), origin.getRow(), origin.getColumn());
+		emptyColor = Color.BLUE;
+	}
+
+}
